aa5edc8c0fed865ee17f92287c52b14a83e8eecc
Karim <karim.sagandykov.2@gmail.com>
2024-01-10 17:17:56 +0100

    Srpint2

--- main.java
+++ main.java
@@ -1,107 +1,85 @@
 import java.util.*;
+import java.time.LocalTime;
 
 public class main {
 
-    public void showmenu(){
+    public static void showMenu() {
+        LocalTime now = LocalTime.now();
+        System.out.println("Current time is " + now.toString().substring(0, 8));
         System.out.println("What kind of cab would you like to order?");
         System.out.println("1.- Regular");
         System.out.println("2.- Special");
         System.out.println("3.- Exit");
         System.out.println("Enter the number of the desired option: ");
     }
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-// VARIABLES-------------------------------------------------------------------------------------------
-        boolean dayShift = false, nightShift = false;
-        int hours, minutes, taxiRegular;
-        int taxiSpecial = 0;
-        Random rand = new Random();
-        String[] carmodels = {"Toyota", "Honda", "Ford", "Nissan", "Chevrolet"};
-        String[] taxi_status = {"O", "F", "B"};
-        int numberTaxis = 50;
-        int menuoption = 0;
-        char neededTaxi;
-        int contfree = 0;
-
-        /*  ------ ASK IF ITS DAY OR NIGHT  ------------*/
-        System.out.println("Hello, please select the current time. (Day shift: from 08h to 22h and Night shift from 22h to 08h). Hours and minutes have to be separated by :. Example - hh:mm");
-
-        while (true){
-            String time = input.nextLine();
-            try {
-                int separate = time.indexOf(':');
-                hours = Integer.parseInt(time.substring(0, separate));
-                minutes = Integer.parseInt(time.substring(separate+1));
-                if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60)break;else {
-                    System.out.println("Something went wrong. Please try again: ");
-                }
-            }catch (Exception e){
-                System.out.println("Something went wrong. Please try again: ");
+    private static void displayTaxis(String[][] taxis, String type) {
+        int countFree = 0;
+        System.out.println(Arrays.toString(taxis[0]));
+        for (int i = 1; i < taxis.length; i++) {
+            if (taxis[i][3].equals(type) && taxis[i][2].equals("F")) {
+                System.out.println(Arrays.toString(taxis[i]));
+                countFree++;
             }
         }
+        System.out.println("There are " + countFree + " " + (type.equals("R") ? "regular" : "special") + " taxis");
+    }
 
-
-        // DEFINE NUM TAXIS PER DAY AND NIGHT
-
-
-        if (hours >= 8 && hours < 22){
-            dayShift = true;
-        } else{
-            nightShift = true;
-            numberTaxis /= 2;
-        }
-
+    private static String[][] taxis(int numberTaxis){
+        Random rand = new Random();
+    
         String[][] taxis = new String[numberTaxis][4];
+
+        String[] carModels = {"Toyota", "Honda", "Ford", "Nissan", "Chevrolet"};
+        String[] taxiStatus = {"O", "F", "B"};
         taxis[0] = new String[]{"ID", "Car Model", "Taxi_status", "Taxi_Type"};
 
-        for (int i = 1; i <= taxis.length-1; i++) {
+        for (int i = 1; i < taxis.length; i++) {
             taxis[i][0] = Integer.toString(i);
-            taxis[i][1] = carmodels[rand.nextInt(carmodels.length)];
-            taxis[i][2] = taxi_status[rand.nextInt(taxi_status.length)];
-            if (i % 10 == 0){
-                taxis[i][3] = "S";
-            } else {
-                taxis[i][3] = "R";
-            }
-        }
+            taxis[i][1] = carModels[rand.nextInt(carModels.length)];
+            taxis[i][2] = taxiStatus[rand.nextInt(taxiStatus.length)];
+            taxis[i][3] = (i % 10 == 0) ? "S" : "R";
+         }
+        return taxis;
+    }
 
-        //MENU
+    private static int getCurrentTime(){
+        LocalTime now = LocalTime.now();
+        int hours = now.getHour();
+        return hours;
+    }
+
+    private static StringBuilder[][] createMap(){
+        StringBuilder[][] map = new StringBuilder[20][20];
+        return map;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int hours = getCurrentTime();
+
+        boolean dayShift = hours >= 8 && hours < 22;
+        int numberTaxis = dayShift ? 50 : 25;
+
+        String[][] taxis = taxis(numberTaxis);
+
+        // Menú
+        int menuOption;
         do {
-            main menu = new main();
-            menu.showmenu();
-            menuoption = input.nextInt();
-            switch (menuoption){
+            showMenu();
+            menuOption = input.nextInt();
+            switch (menuOption) {
                 case 1:
-                    System.out.println(Arrays.toString(taxis[0]));
-                    for (int i = 1; i <= taxis.length-1; i++) {
-                        if (taxis[i][3].equals("R")) {
-                            if (taxis[i][2].equals("F")) {
-                                System.out.println(Arrays.toString(taxis[i]));
-                                contfree++;
-                            }
-                        }
-                    }
-                    System.out.println("There are " + contfree + " regular taxis");
+                    displayTaxis(taxis, "R");
                     break;
                 case 2:
-                    for (int i = 0; i < taxis.length; i++) {
-                        if (taxis[i][3].equals("S")) {
-                            if (taxis[i][2].equals("F")) {
-                                System.out.println(Arrays.toString(taxis[i]));
-                                contfree++;
-                            }
-                        }
-                    }
-                    System.out.println("There are " + contfree + " special taxis");
+                    displayTaxis(taxis, "S");
                     break;
-
                 case 3:
                     System.out.println("Byebye");
                     break;
             }
-        } while (menuoption != 3);
+        } while (menuOption != 3);
     }
+
 
 }
